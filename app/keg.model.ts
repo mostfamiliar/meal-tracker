e637a3cdc5190654d607b2d9153b939938dc1d09
torrence stratton <tlstratton@gmail.com>
2016-03-25 07:56:12 -0700

    add initial setup files

--- app/keg.model.ts
+++
@@ -1,7 +0,0 @@
-// //example of a 'master' Class
-export class Keg {
-  public pintsLeft: number = 124;
-  constructor(public name: string, public brand: string, public pintCost: number, public abv: number){
-
-  }
-}
---
+++ app/meal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from './angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'meal-display',
+  inputs: ['meal'],
+  template: `
+  <div>
+  <h3>test</h3>
+  </div>
+  `
+})
+
+export class MealComponent {
+  public meal: Meal;
+}
